75d469b4057e341b3db3ff070b16556039a3f845
Mike Murray <mikemurray056@gmail.com>
2021-06-17 14:48:12 -0400

    More script work

--- Assets/Scripts/CharacterClass.cs
+++ Assets/Scripts/CharacterClass.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //this is the base class for all character classes in the game.
-public abstract class CharacterClass
+public class CharacterClass
 {
-    protected int Level { get; set; }           //LV for short
-    protected int HealthPoints { get; set; }    //HP
-    protected int ManaPoints { get; set; }      //MP
-    protected int AttackPower { get; set; }     //ATP for short
-    protected int DefensePower { get; set; }    //DFP
-    protected int[] ElementalPower { get; set; }  //ELP. There are five elements.
-    protected int ExperiencePoints { get; set; }    //XP. Max is always 1000 XP
+    public int Level { get; set; }           //LV for short
+    public int HealthPoints { get; set; }    //HP
+    public int[] ManaPoints { get; set; }      //MP
+    public int AttackPower { get; set; }     //ATP for short
+    public int DefensePower { get; set; }    //DFP
+    public int[] ElementalPower { get; set; }  //ELP. There are five elements.
+    public int ExperiencePoints { get; set; }    //XP. Max is always 1000 XP
 
-    protected enum Element
+    public enum Element
     {
         Fire,       //red
         Water,      //blue
@@ -22,6 +22,6 @@ public abstract class CharacterClass
         Shadow      //purple
     }
 
-    protected List<string> Skills { get; set; }     //TODO: should be a collection of Skill objects instead of string
+    public List<string> Skills { get; set; }     //TODO: should be a collection of Skill objects instead of string
 
 }
--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill : MonoBehaviour
+//base class for all skills in the game
+public class Skill
 {
-    // Start is called before the first frame update
-    void Start()
+   protected string Name { get; set; }
+   protected int[] manaCost { get; set; }  //5 different values, one for each colour (element)
+   protected float Cooldown { get; set; }   //How much time must pass before player can use skill again
+
+    protected float CurrentTime { get; set; }
+    protected enum SkillType
     {
-        
+        Active,     
+        Passive     //passives have no cost and are always on.
     }
 
-    // Update is called once per frame
-    void Update()
+    //constants
+    const int MAX_ELEMENTS = 5;
+
+    public Skill(string name, int[] cost, float cooldown)
     {
-        
+        manaCost = new int[MAX_ELEMENTS];
+        Name = name;
+        Cooldown = cooldown;
+
+        for (int i = 0; i < MAX_ELEMENTS; i++)
+        {
+            manaCost[i] = cost[i];
+        }
+    }
+
+    protected bool CanUseSkill(CharacterClass player)
+    {
+        bool skillAvailable = false;
+
+        if (Time.time > CurrentTime + Cooldown)
+        {
+            int matchCount = 0;
+            //check if player has required mana
+            for (int i = 0; i < MAX_ELEMENTS; i++)
+            {
+                if (player.ManaPoints[i] >= manaCost[i])
+                    matchCount++;
+            }
+
+            //Must have a complete match for skill to be available
+            if (matchCount >= MAX_ELEMENTS)
+                skillAvailable = true;
+        }
+
+        return skillAvailable;
+    }
+
+    // The following method must be overloaded because different skills have different targets.
+    protected virtual void UseSkill(CharacterClass target)
+    {
+        //get timestamp to start the cooldown
+        CurrentTime = Time.time;
     }
 }
